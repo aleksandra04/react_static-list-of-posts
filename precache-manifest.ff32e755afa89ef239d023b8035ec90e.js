e078214c990d60e3db2d5e1f66ecfc717418a6df
Alexandra Boyko <Alexboyko04@gmail.com>
2019-12-05 11:37:37 +0200

    Updates

---
+++ precache-manifest.ff32e755afa89ef239d023b8035ec90e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f220989dba8e4f1562c52f9e85ba464f",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "29276b02cfbb4c6203c5",
+    "url": "/react_static-list-of-posts/static/css/main.80183332.chunk.css"
+  },
+  {
+    "revision": "3485fa9145fb43f72c73",
+    "url": "/react_static-list-of-posts/static/js/2.506750e1.chunk.js"
+  },
+  {
+    "revision": "29276b02cfbb4c6203c5",
+    "url": "/react_static-list-of-posts/static/js/main.ca32e9a5.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
